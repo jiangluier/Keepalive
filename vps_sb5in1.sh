24b647a07a78a4be483e0e58a1796aaf117f32bc
yutian81 <yutian88881@hotmail.com>
2025-09-19 10:38:05 +0800

    Update vps_sb5in1.sh

--- vps_sb5in1.sh
+++ vps_sb5in1.sh
@@ -712,7 +712,7 @@ create_shortcut() {
   cat > "$work_dir/sb.sh" << EOF
 #!/usr/bin/env bash
 
-bash <(curl -Ls https://raw.githubusercontent.com/yutian81/Keepalive/main/vps_nat5in1.sh) \$1
+bash <(curl -Ls https://raw.githubusercontent.com/yutian81/Keepalive/main/vps_sb5in1.sh) \$1
 EOF
   chmod +x "$work_dir/sb.sh"
   ln -sf "$work_dir/sb.sh" /usr/bin/sb
